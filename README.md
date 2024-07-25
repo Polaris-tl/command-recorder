d2568fc989c8b10599b39ffacdbc6c4c04d15a61
polaris <polaris66tl@gmail.com>
2024-07-25 22:40:03 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,53 @@
+# Command Recorder
+
+Command Recorder 是一个命令行工具，用于记录在特定目录下执行的命令。下次进入该目录时，只需输入一个简单的命令即可执行上次记录的命令。
+
+## 描述
+
+Command Recorder 允许你记录在特定目录下执行的命令。下次进入该目录时，你可以简单地输入 fuck 来执行上次记录的命令
+
+## 安装
+
+确保你已经安装了 Node.js 和 npm。然后在项目目录下运行以下命令来安装依赖：
+```npm install command-recorder```
+
+## 使用方法
+
+### 版本
+
+```fuck -v```
+
+### 记录命令
+
+记录在指定目录下执行的命令：
+```fuck -c "<your-command>"```
+示例
+```fuck -c "ls -la"```
+
+### 删除特定目录的命令
+
+```fuck -r "<path/to/your/dir>"```
+
+### 删除当前目录的命令记录
+
+```fuck --remove "current"```
+
+### 立即执行命令
+
+你可以选择在记录命令时立即执行它。默认情况下，该选项是启用的：
+```fuck -c "<your-command>" -i "true"```
+如果你不希望立即执行命令，可以将 --immediately 选项设置为 false：
+```fuck -c "<your-command>" -i "false"```
+
+## 选项
+
+- -V, --version ：输出版本号。
+- -c, --command \<value>：你想要记录的命令。
+- -d, --dir \<value>：你想要记录命令的目录，默认为当前目录。
+- -l, --list：列出所有你已经记录过命令的目录。
+- -r, --remove \<value>：删除特定目录的命令记录，current 将删除当前目录的命令记录。
+- -i, --immediately \<value>：立即执行命令，默认为 true。
+
+## 注意事项
+
+确保你在使用该工具时具有相应目录的读写权限。
---
+++ src/index.ts
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander'
+import { spawn } from 'child_process'
+import DataStorage from './dataStorage'
+
+const program = new Command()
+program
+	.version('1.0.0')
+	.description(
+		'Record the command executed in a specific directory, so that the next time you enter that directory, you can simply type "fuck" to execute the previous command'
+	)
+	.option('-c, --command <value>', 'the command you want to record')
+	.option(
+		'-d, --dir <value>',
+		'the directory you want to record the command, default is current directory',
+		process.cwd()
+	)
+	.option('-l, --list', 'list all the directories you have recorded')
+	.option('-r, --remove <value>', `remove a specific directory, 'current' will remove the current directory's command`)
+	.option('-i, --immediately <value>', 'execute the command immediately, default is true', true)
+	.parse(process.argv)
+
+const store = new DataStorage('fuck')
+const options = program.opts()
+const command = options.command
+const currentDir = options.dir
+const immediately = options.immediately
+
+// 输出版本号
+if (options.version) {
+    console.log('1.0.0')
+    process.exit(0)
+}
+
+// 列出所有记录的目录
+if (options.list) {
+	const dirs = store.listItems()
+	console.log('Recorded directories: ', dirs)
+	process.exit(0)
+}
+
+// 删除特定目录的命令
+if (options.remove) {
+	let dir = options.remove
+	if (options.remove === 'current') {
+		dir = currentDir
+	}
+	store.removeItem(dir)
+	console.log('Removed command for directory: ', dir)
+	process.exit(0)
+}
+
+// 设置命令记录
+if (command && currentDir) {
+	store.setItem(currentDir, command)
+	console.log(`Recorded command "${command}" for directory: ${currentDir}`)
+
+	if (immediately) {
+		console.log(`Executing`)
+		exitCommand(command)
+	}
+	process.exit(0)
+}
+
+// 执行上次的命令
+const lastCommand = store.getItem(currentDir)
+if (lastCommand) {
+	console.log(`Executing last command "${lastCommand}" for directory: ${currentDir}`)
+	exitCommand(lastCommand)
+} else {
+	console.log(`No command recorded for directory: ${currentDir}`)
+}
+
+function exitCommand(code: string) {
+    if (!code) {
+        console.error(`Command must be specified`)
+        process.exit(1)
+    }
+
+    const child = spawn(command, {
+        shell: true,
+        stdio: 'inherit',
+    })
+
+    child.on('error', error => {
+        console.error(`Error executing command: ${error.message}`)
+    })
+
+    child.on('close', code => {
+        if (code !== 0) {
+            console.error(`Command failed with code ${code}`)
+        }
+        process.exit(code)
+    })
+    child.on('exit', code => {
+        if (code !== 0) {
+            console.error(`Command failed with code ${code}`)
+        }
+        process.exit(code)
+    })
+}
